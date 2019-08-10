5b458f684091d88c25f706eb8ca31a75a0e6772d
someone <something@apa.com>
2019-08-10 05:26:34 -0500

    wrote basic tests for sharpen()

--- actions/sharpen.spec.js
+++ actions/sharpen.spec.js
@@ -1,1 +1,19 @@
 const sharpen = require("./sharpen");
+let pencil = {};
+
+describe("sharpen()", () => {
+  describe("sharpens the pencil", () => {
+    beforeEach(() => {
+      pencil = { maxPoint: 100, point: 0, length: 2 };
+      sharpen(pencil);
+    });
+
+    test("restore the pencil's to its initial point durability", () => {
+      expect(pencil.point).toBe(pencil.maxPoint);
+    });
+
+    test("decrease the pencil's length by 1", () => {
+      expect(pencil.length).toBe(1);
+    });
+  });
+});
