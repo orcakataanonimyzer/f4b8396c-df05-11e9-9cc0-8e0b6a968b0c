33a1f0dec25cfee7d7c16fa69c5cec45fbca87c6
someone <something@apa.com>
2019-08-10 08:41:30 -0500

    added test file for erase()

---
+++ actions/erase.spec.js
@@ -0,0 +1,17 @@
+// const sharpen = require("./sharpen");
+let pencil = {};
+
+describe("erase()", () => {
+  // describe("sharpens the pencil", () => {
+  //     beforeEach(() => {
+  //         pencil = { maxPoint: 100, point: 0, length: 2 };
+  //         sharpen(pencil);
+  //     });
+  //     test("restore the pencil's to its initial point durability", () => {
+  //         expect(pencil.point).toBe(pencil.maxPoint);
+  //     });
+  //     test("decrease the pencil's length by 1", () => {
+  //         expect(pencil.length).toBe(1);
+  //     });
+  // });
+});
