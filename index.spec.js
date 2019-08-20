c243b1a76b6dda6f32e0e2cb69094a522851ed4e
someone <something@apa.com>
2019-08-20 09:44:22 -0500

    added write() test for PencilSimulator class

--- index.spec.js
+++ index.spec.js
@@ -55,10 +55,42 @@ describe("PencilSimulator class", () => {
     });
 
     describe("successfull initializations", () => {
-      test("create a pencil simulator", () => {
-        const pencil = new PencilSimulator(10, 10, 10);
+      let pencil = null;
+      beforeEach(() => {
+        pencil = new PencilSimulator(10, 10, 10);
+      });
+      test("create a pencil simulator object", () => {
         expect(typeof pencil).toBe("object");
       });
+      test("pencil must have length", () => {
+        expect(pencil.length).toBe(10);
+      });
+      test("pencil must have max point durability", () => {
+        expect(pencil.maxPoint).toBe(10);
+      });
+      test("pencil must have point durability", () => {
+        expect(pencil.point).toBe(10);
+      });
+      test("pencil must have eraser durability", () => {
+        expect(pencil.eraser).toBe(10);
+      });
+    });
+  });
+
+  describe("write", () => {
+    let pencil = null;
+    let paper = null;
+    beforeEach(() => {
+      pencil = new PencilSimulator(10, 100, 10);
+      paper = { content: "" };
+      pencil.write(paper, "I am writing my first sentence");
+    });
+
+    test("write the 1st sentence on the paper", () => {
+      expect(paper.content).toBe("I am writing my first sentence");
+    });
+    test("degrade point durability", () => {
+      expect(pencil.point).toBe(100 - 23);
     });
   });
 });
